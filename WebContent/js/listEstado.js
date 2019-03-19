57779b6657f72aa5f3db10ba1eaab6fb43f9ee38
Senai <Senai@SNLDN09LABF04>
2019-03-19 15:46:07 -0300

    commit

--- WebContent/js/listEstado.js
+++ WebContent/js/listEstado.js
@@ -1,21 +1,118 @@
-$(document).ready(function() {
+$(document)
+		.ready(
+				function() {
 
-	$('#btn-salvar').on('click',function(){
-		
-		$.ajax({
-			url: 'ListarEstadoServlet',
-			type: 'POST',
-			data:{
-				'nome-estado' : $('#tf_estado').val(),
-				'sigla-estado' : $('#tf_sigla').val(),
-				'status-estado' : $('#slc-status').val()				
-			}, success: function(response){
-				
-			}
-		})
-		
-		
-	});
-	
-	
-});
+					$.ajax({
+						url : 'ListarEstadoServlet',
+						type : 'POST',
+						success : function(response) {
+							// alert(JSON.stringify(response));
+							render(response);
+
+						}
+					})
+
+					function render(data) {
+						var tabela = $('#set').append(
+								$('<table></table>').attr("id", "tabela"));
+						var tHead = "<thead id='tableHead'></thead>";
+						$('#tabela').append(tHead);
+						var rowH = "<tr> " + "<th>ID</th>" + "<th>Nome</th>"
+								+ "<th>Sigla</th>" + "<th>Status</th>"
+								+ "<th>Actions</th>" + "</tr>";
+						$('#tableHead').append(rowH);
+
+						$('#tabela').append(
+								$('<tbody></tbody>').attr("id", "tabelaBody"));
+
+						$
+								.each(
+										data,
+										function(index, value) {
+
+											var td = $("<tr id= 'row" + index
+													+ "'>" + "</tr>");
+
+											var row1 = $("<td>"
+													+ value.idEstado + "</td>");
+
+											var row2 = $("<td>"
+													+ value.nomeEstado
+													+ "</td>");
+
+											var row3 = $("<td>"
+													+ value.siglaEstado
+													+ "</td>");
+
+											var row4 = $("<td>"
+													+ value.statusEstado
+													+ "</td>");
+
+											var button = $(
+													'<button>Modificar</button>')
+													.attr(
+															"id",
+															"btn-update-"
+																	+ index)
+													.click({
+														p1 : this
+													}, setInputData);
+
+											var row5 = $("<td></td>");
+											row5.append(button);
+
+											td.append(row1);
+											td.append(row2);
+											td.append(row3);
+											td.append(row4);
+											td.append(row5);
+
+											$('#tabela tbody').append(td);
+
+											$('#set')
+													.append('</tbody></table>');
+
+										})
+
+					}
+
+					function setInputData(event) {
+
+						console.log($('#row'));
+
+						var row = "#" + $(this).parent().parent().attr('id');
+
+						var idEstado = $(row).find("td:eq(0)").text();
+
+						var nomeEstado = $(row).find("td:eq(1)").text();
+
+						var siglaEstado = $(row).find("td:eq(2)").text();
+
+						var statusEstado = $(row).find("td:eq(3)").text();
+
+						// event.data.p1 =
+						// $(row).find("td:eq(3)").children().text();
+
+
+						var opt = $('#slc_status option').text();
+						alert(opt);
+
+						// alert($(this).parent().parent().attr('id'));
+
+						clearFields();
+						$('#tf_id').val(idEstado);
+						$('#tf_estado').val(nomeEstado);
+						$('#tf_uf').val(siglaEstado);
+						$('#slc_status').val(statusEstado).attr('selected',
+								'selected');
+
+					}
+
+					function clearFields() {
+						$('#tf_id').val(null);
+						$('#tf_estado').val(null);
+						$('#tf_uf').val(null);
+						$('#slc_status').val(null);
+					}
+
+				});
--- WebContent/listar_estado.jsp
+++ WebContent/listar_estado.jsp
@@ -1,18 +1,27 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
-    pageEncoding="ISO-8859-1"%>
+	pageEncoding="ISO-8859-1"%>
 <!DOCTYPE html>
 <html>
 <head>
 <meta charset="ISO-8859-1">
-<title>Insert title here</title>
+<title>Listar Estados</title>
 <script src="js/jquery-3.3.1.js"></script>
 <script src="js/listEstado.js"></script>
+<link rel="stylesheet" type="text/css" href="css/style.css" />
 </head>
 <body>
 
-<div id="set">
-</div>
-
-
+	<div id="set"></div>
+	<div id="form">
+		<form method="post" action="AtualizarEstadoServlet">
+			<input type = "text" disabled id="tf_id"/>
+			<input type="text" placeholder="Estado" id="tf_estado" /> <input
+				type="text" placeholder="uf" id="tf_uf" /> <select
+				name="slc_status">
+				<option value=0>Ativo</option>
+				<option value=1>Inativo</option>
+			</select> <input type="button" value="Salvar" id="btn-salvar" />
+		</form>
+	</div>
 </body>
 </html>
